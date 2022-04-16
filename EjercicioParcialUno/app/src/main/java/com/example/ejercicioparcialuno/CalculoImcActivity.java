42748ef36e6a7f781462829417def1b56d930403
AngelaEspinosa <titagena@gmail.com>
2022-04-15 21:18:24 -0500

    Inicio en la configuracion del SharedPreferences en MenuPpal y CalculoImcActivity.java

--- EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/CalculoImcActivity.java
+++ EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/CalculoImcActivity.java
@@ -2,7 +2,10 @@ package com.example.ejercicioparcialuno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class CalculoImcActivity extends AppCompatActivity {
 
@@ -10,6 +13,9 @@ public class CalculoImcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculo_imc);
+        SharedPreferences spGet = getSharedPreferences("nombreUsuario", MODE_PRIVATE);
+        String name = spGet.getString("Nombre", "NA");
+        Toast.makeText(getApplicationContext(), "Hola "+name, Toast.LENGTH_LONG).show();
     }
 
 
