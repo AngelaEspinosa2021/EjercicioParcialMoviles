8cb0756c11d29171f8767aafde415bc28dd64abf
AngelaEspinosa <titagena@gmail.com>
2022-04-19 17:01:04 -0500

    Se crea la vista donde se visualizarán los resultados del Calculo de IMC.

---
+++ EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/ResultadoImcActivity.java
@@ -0,0 +1,14 @@
+package com.example.ejercicioparcialuno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultadoImcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_imc);
+    }
+}
