102cc6bc46f34abccd2b1ce6a58b9fb309d5d340
AngelaEspinosa <titagena@gmail.com>
2022-04-22 14:32:15 -0500

    Se hace la referenciacion de cada boton y se crea a cada uno el onClick.

--- EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/CalculadoraActivity.java
+++ EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/CalculadoraActivity.java
@@ -4,25 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class CalculadoraActivity extends AppCompatActivity {
+public class CalculadoraActivity extends AppCompatActivity implements View.OnClickListener{
 
     public TextView nombreUsuarioCalc;
     public EditText pantallaCalculadora;
     public Button uno, dos, tres, cuatro, cinco, seis, siete,ocho, nueve, cero;
-    public Button suma, resta, multiplicacion, division;
+    public Button suma, resta, multiplicacion, division, total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
         setContentAtributes();
+        setContentViewEvents();
         SharedPreferences spGet4 = getSharedPreferences("nombreUsuario", MODE_PRIVATE);
         String name = spGet4.getString("Nombre", "");
         nombreUsuarioCalc.setText(name);
+    }
+
+    private void setContentViewEvents() {
+        uno.setOnClickListener(this);
+        dos.setOnClickListener(this);
+        tres.setOnClickListener(this);
+        cuatro.setOnClickListener(this);
+        cinco.setOnClickListener(this);
+        seis.setOnClickListener(this);
+        ocho.setOnClickListener(this);
+        nueve.setOnClickListener(this);
+        cero.setOnClickListener(this);
+        suma.setOnClickListener(this);
+        resta.setOnClickListener(this);
+        multiplicacion.setOnClickListener(this);
+        division.setOnClickListener(this);
+        total.setOnClickListener(this);
     }
 
     private void setContentAtributes() {
@@ -42,5 +61,102 @@ public class CalculadoraActivity extends AppCompatActivity {
         resta = findViewById(R.id.resta);
         multiplicacion = findViewById(R.id.multiplicacion);
         division = findViewById(R.id.division);
+        total = findViewById(R.id.total);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.uno:
+                goToUnoAction();
+                break;
+            case R.id.dos:
+                goToDosAction();
+                break;
+            case R.id.tres:
+                goToTresAction();
+                break;
+            case R.id.cuatro:
+                goToCuatroAction();
+                break;
+            case R.id.cinco:
+                goToCincoAction();
+                break;
+            case R.id.seis:
+                goToSeisAction();
+                break;
+            case R.id.siete:
+                goToSieteAction();
+                break;
+            case R.id.ocho:
+                goToOchoAction();
+                break;
+            case R.id.nueve:
+                goToNueveAction();
+                break;
+            case R.id.cero:
+                goToCeroAction();
+                break;
+            case R.id.suma:
+                goToSumaAction();
+                break;
+            case R.id.resta:
+                goToRestaAction();
+                break;
+            case R.id.multiplicacion:
+                goToMultiplicacionAction();
+                break;
+            case R.id.division:
+                goToDivisionAction();
+                break;
+            case R.id.total:
+                goToTotalAction();
+                break;
+        }
+    }
+
+    private void goToTotalAction() {
+    }
+
+    private void goToDivisionAction() {
+    }
+
+    private void goToMultiplicacionAction() {
+    }
+
+    private void goToRestaAction() {
+    }
+
+    private void goToSumaAction() {
+    }
+
+    private void goToCeroAction() {
+    }
+
+    private void goToNueveAction() {
+    }
+
+    private void goToOchoAction() {
+    }
+
+    private void goToSieteAction() {
+    }
+
+    private void goToSeisAction() {
+    }
+
+    private void goToCincoAction() {
+    }
+
+    private void goToCuatroAction() {
+    }
+
+    private void goToTresAction() {
+    }
+
+    private void goToDosAction() {
+    }
+
+    private void goToUnoAction() {
     }
 }
--- EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/Operaciones.java
+++ EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/Operaciones.java
@@ -1,2 +1,29 @@
-package com.example.ejercicioparcialuno;public class Operaciones {
+package com.example.ejercicioparcialuno;
+
+public class Operaciones {
+
+    int numero;
+    int numeroAcum;
+
+    public int suma(int numero){
+        numeroAcum = numeroAcum + numero;
+        return numeroAcum;
+    }
+
+    public int resta(int numero){
+        numeroAcum = numeroAcum - numero;
+        return numeroAcum;
+    }
+
+    public int multiplicacion(int numero){
+        numeroAcum = numeroAcum * numero;
+        return numeroAcum;
+    }
+
+    public int division(int division){
+        numeroAcum = numeroAcum / numero;
+        return numeroAcum;
+
+    }
+
 }
