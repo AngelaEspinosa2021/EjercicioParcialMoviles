dc270368722b7a3b263c54525109b38191e125bf
AngelaEspinosa <titagena@gmail.com>
2022-04-22 14:15:43 -0500

    Se crean cada boton de las operaciones basicas incluyendo el boton para el total.

--- EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/CalculadoraActivity.java
+++ EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/CalculadoraActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,6 +12,8 @@ public class CalculadoraActivity extends AppCompatActivity {
 
     public TextView nombreUsuarioCalc;
     public EditText pantallaCalculadora;
+    public Button uno, dos, tres, cuatro, cinco, seis, siete,ocho, nueve, cero;
+    public Button suma, resta, multiplicacion, division;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,19 @@ public class CalculadoraActivity extends AppCompatActivity {
     private void setContentAtributes() {
         nombreUsuarioCalc = findViewById(R.id.nombreUsuarioCalc);
         pantallaCalculadora = findViewById(R.id.pantallaCalculadora);
+        uno = findViewById(R.id.uno);
+        dos = findViewById(R.id.dos);
+        tres = findViewById(R.id.tres);
+        cuatro = findViewById(R.id.cuatro);
+        cinco = findViewById(R.id.cinco);
+        seis = findViewById(R.id.seis);
+        siete = findViewById(R.id.siete);
+        ocho = findViewById(R.id.ocho);
+        nueve = findViewById(R.id.nueve);
+        cero = findViewById(R.id.cero);
+        suma = findViewById(R.id.suma);
+        resta = findViewById(R.id.resta);
+        multiplicacion = findViewById(R.id.multiplicacion);
+        division = findViewById(R.id.division);
     }
 }
