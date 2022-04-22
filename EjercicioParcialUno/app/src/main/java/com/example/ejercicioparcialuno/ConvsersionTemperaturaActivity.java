bd8a095ed4e5f9d4d9eb126d6eb62b14c91857ec
AngelaEspinosa <titagena@gmail.com>
2022-04-22 10:25:36 -0500

    Se configura el sharedPreferencias para mostrar el nombre del usuario.

--- EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/ConvsersionTemperaturaActivity.java
+++ EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/ConvsersionTemperaturaActivity.java
@@ -2,13 +2,25 @@ package com.example.ejercicioparcialuno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConvsersionTemperaturaActivity extends AppCompatActivity {
+    public TextView nombreUsuarioTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convsersion_temperatura);
+        setContentAtributes();
+        SharedPreferences spGet = getSharedPreferences("nombreUsuario", MODE_PRIVATE);
+        String name = spGet.getString("Nombre", "");
+        nombreUsuarioTemp.setText(name);
+    }
+
+    private void setContentAtributes() {
+        nombreUsuarioTemp=findViewById(R.id.nombreUsuarioTemp);
+
     }
 }
