71e635082ec7c13fbceab27dd766fc9d62b26cc6
AngelaEspinosa <titagena@gmail.com>
2022-04-22 04:34:41 -0500

    clase Imc con sus métodos

--- EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/Imc.java
+++ EjercicioParcialUno/app/src/main/java/com/example/ejercicioparcialuno/Imc.java
@@ -1,2 +1,60 @@
-package com.example.ejercicioparcialuno;public class Imc {
+package com.example.ejercicioparcialuno;
+
+public class Imc {
+    double peso;
+    double altura;
+    double resultado;
+    String[] niveles;
+    String rango;
+    String riesgo;
+
+    public Imc(double peso, double altura){
+        this.peso=peso;
+        this.altura=altura;
+    }
+
+    public double calcularImc(double peso, double altura){
+        resultado = (peso) /(altura*altura);
+        return resultado;
+    }
+
+    public String[] validarRango(double resultado){
+
+        if(resultado >= 18.5 && resultado <= 24.9) {
+            rango = "Normal";
+            riesgo = "Promedio";
+            String[] niveles = {rango,riesgo};
+            return niveles;
+        }
+        if(resultado >= 25 && resultado <= 29.9)
+        {
+            rango="Sobrepeso";
+            riesgo="Aumentado";
+            String[] niveles = {rango,riesgo};
+            return niveles;
+        }
+        if(resultado >= 30 && resultado <= 34.9)
+        {
+            rango="Obesidad grado I";
+            riesgo="Moderado";
+            String[] niveles = {rango,riesgo};
+            return niveles;
+        }
+        if(resultado >= 35 && resultado <= 39.9)
+        {
+            rango="Obesidad grado II";
+            riesgo="Severo";
+            String[] niveles = {rango,riesgo};
+            return niveles;
+        }
+        if(resultado> 40)
+        {
+            rango="Obesidad grado III";
+            riesgo="Muy Severo";
+            String[] niveles = {rango,riesgo};
+            return niveles;
+        }
+
+        return null;
+    }
 }
